482576229a35e8e086634b444915ecf1826d6433
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-04-27 16:31:09 -0400

    Add files via upload

--- gateway/device.js
+++ gateway/device.js
@@ -1,1 +1,35 @@
+'use strict';
 
+const m2m = require('m2m');
+
+let device = new m2m.Device(1200);
+
+let server = 1;
+
+function loadServer(id, data){
+  device.getData({id:id, channel:'test-data'}, (d) => {
+    if(d){
+      setImmediate(() => {
+        data.send(d);
+      });
+    }
+  });
+}
+
+device.connect(() => {
+  // Set server balancer 1200
+  device.setChannelData('sb-1200', (data) => { 
+    if(server === 1){
+      server = 2;
+      loadServer(100, data);
+    }
+    else if(server === 2){
+      server = 3;
+      loadServer(200, data);
+    }
+    else if(server === 3){
+      server = 1;
+      loadServer(300, data);
+    }
+  });
+});
