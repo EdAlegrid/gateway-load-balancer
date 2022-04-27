d686cf25c62788ccb7a164392a8de2b9a2b769f4
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-04-27 16:32:13 -0400

    Add files via upload

--- server2/device.js
+++ server2/device.js
@@ -1,1 +1,11 @@
+'use strict';
 
+const m2m = require('m2m');
+
+let server = new m2m.Device(200);
+
+server.connect(() => {
+  server.setChannelData('test-data', (data) => {
+    data.send({msg:'hello from server 200'});
+  });
+});
