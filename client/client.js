c13d459c96cbbab1c1181d109bddfd1049d8901d
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-04-28 11:21:28 -0400

    Update client.js

--- client/client.js
+++ client/client.js
@@ -5,14 +5,6 @@ const m2m = require('m2m');
 let client = new m2m.Client();
 
 client.connect(() => {
-  /*setInterval(() => {
-    client.getChannelData({id:1200, channel:'sb-1200'}, (data) => {
-      console.log('get gateway sb-1200', data);
-    });
-  }, 25000);*/
-
-  // or 
-
   client.watchChannelData({id:1200, channel:'sb-1200'}, (data) => {
     console.log('watch gateway sb-1200', data);
   });
